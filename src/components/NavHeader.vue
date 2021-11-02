ecab1aa6d334407ae5925b1a947ee165203ccde1
1151309124 <1151309124@qq.com>
2021-11-02 15:34:35 +0800

    header2

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-10-28 14:44:11
- * @LastEditTime: 2021-11-02 15:11:42
+ * @LastEditTime: 2021-11-02 15:30:35
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \htmle:\vue项目\mimall\src\components\NavHeader.vue
@@ -10,20 +10,15 @@
     <div class="header">
         <div class="nav-topbar">
             <div class="container">
-                <div class="topbar-menu">
-                    <a href="javascript:;">小米商城</a>
-                    <a href="javascript:;">MUI</a>
-                    <a href="javascript:;">云服务</a>
-                    <a href="javascript:;">协议规则</a>
-                </div>
-                <div class="topbar-user">
-                    <a href="javascript:;">登录</a>
-                    <a href="javascript:;">注册</a>
-                    <a href="javascript:;" class="my-cart"><span class="icon-cart"></span> 购物车</a>
-                </div>
             </div>
         </div>
-        <div class="nav-header"></div>
+        <div class="nav-header">
+            <div class="container">
+                <div class="header-logo"></div>
+                <div class="header-menu"></div>
+                <div class="header-search"></div>
+             </div>
+        </div>
     </div>
 </template>
 <script>
