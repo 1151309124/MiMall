7701c6350d77810eb889e46d8b30e5ab583db83e
1151309124 <1151309124@qq.com>
2021-11-02 21:48:10 +0800

    chage

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -1,21 +1,80 @@
 <!--
  * @Author: your name
  * @Date: 2021-10-28 14:44:11
- * @LastEditTime: 2021-10-28 20:11:13
+ * @LastEditTime: 2021-11-02 21:47:36
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \htmle:\vue项目\mimall\src\components\NavHeader.vue
 -->
 <template>
-    <div>header</div>
+    <div class="header">
+        <div class="nav-topbar">
+            <div class="container">
+                <div class="topbar-menu">
+                    <a href="javascript:;">小米商城</a>
+                    <a href="javascript:;">MUI</a>
+                    <a href="javascript:;">云服务</a>
+                    <a href="javascript:;">协议规则</a>
+                </div>
+                <div class="topbar-user">
+                    <a href="javascript:;">登录</a>
+                    <a href="javascript:;">注册</a>
+                    <a href="javascript:;" class="my-cart"><span class="icon-cart"></span> 购物车</a>
+                </div>
+            </div>
+        </div>
+        <div class="nav-header"></div>
+    </div>
 </template>
 <script>
 export default {
     name:'nav-header',
     
 }
+/* 
+sass-loader 4.1.1，node-sass 4.3.0
+sass-loader 7.0.3，node-sass 4.7.2
+sass-loader 7.3.1，node-sass 4.7.2
+sass-loader 7.3.1，node-sass 4.14.1 
+sass-loader和node-sass要对应安装，反正现在是有了，上面是各个版本对应的
+*/
 </script>
 
-<style>
-
+<style lang="scss">
+    .header{
+        .nav-topbar{
+            height: 39px;
+            line-height: 39px;
+            background-color: #333333;
+            // color: #B0B0B0;
+        }
+        .container{
+            width: 1226px;   
+            margin-right: auto;
+            margin-left: auto;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            a{
+                display: inline-block;
+                color: #B0B0B0;
+                margin-right:17px ;
+            }
+            .my-cart{
+                width: 110px;
+                background-color: #FF6600;
+                text-align: center;
+                color: #ffffff;
+                .icon-cart{
+                display: inline-block;
+                width: 16px;
+                height: 12px;
+                background: url('/imgs/icon-cart-checked.png')no-repeat center;
+                background-size: contain;
+                margin-right: 4px;
+                }
+            }
+        }
+    }
+     
 </style>
