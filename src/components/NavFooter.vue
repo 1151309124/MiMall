6b47be6be6164b4e22815ac4d97955c91322239b
1151309124 <1151309124@qq.com>
2021-11-04 14:40:49 +0800

    bug

--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -1,15 +1,9 @@
 <!--
  * @Author: your name
  * @Date: 2021-10-28 14:52:51
-<<<<<<< HEAD
-<<<<<<< HEAD
- * @LastEditTime: 2021-11-02 14:21:21
-=======
- * @LastEditTime: 2021-11-02 21:55:47
->>>>>>> Nav-Header-1
-=======
+ * @LastEditTime: 2021-11-04 14:38:19
+ * @LastEditTime: 2021-11-02 21:55:
  * @LastEditTime: 2021-11-04 14:07:05
->>>>>>> Nav-Header-1
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \htmle:\vue项目\mimall\src\components\NavFooter.vue
